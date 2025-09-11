137e3863789b13a35111adddd2ee32be2aca229c
tsavasci <tsavasci@yahoo.com>
2025-09-11 19:48:41 +0300

    Initial Commit

---
+++ LGS Kocum PRO/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  LGS Kocum PRO
+//
+//  Created by Tamer Savaşcı on 11.09.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ LGS Kocum PRO/LGS_Kocum_PROApp.swift
@@ -0,0 +1,32 @@
+//
+//  LGS_Kocum_PROApp.swift
+//  LGS Kocum PRO
+//
+//  Created by Tamer Savaşcı on 11.09.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LGS_Kocum_PROApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
